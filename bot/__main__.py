50b5fc78bc32a042add8cf80486764034d4dbd24
totaflix <starastar230@gmail.com>
2024-11-08 17:25:26 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -263,7 +263,7 @@ async def main():
         BotCommands.StatsCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
     LOGGER.info(f"WZML-X Bot [@{bot_name}] Started!")
     if user:
-        LOGGER.info(f"WZ's User [@{user.str.username}] Ready!")
+        LOGGER.info(f"WZ's User [@{user.username}] Ready!")
     signal(SIGINT, exit_clean_up)
 
 async def stop_signals():
